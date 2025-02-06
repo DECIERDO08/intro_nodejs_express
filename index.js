351407db96252c2ca5319be58c006adf6fbc62b0
DECIERDO08 <jingleotome@gmail.com>
2025-02-06 09:13:21 -0800

    Updated everything: Project Done

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json()); //Middleware to parse JSON bodies
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 //Define a route for the home page
 app.get('/', (req, res) => {
     res.send('Hello, World!');
@@ -11,15 +18,34 @@ app.get('/about', (req, res) => {
     res.send('About Us');
 });
 
+const items = ['Apple', 'Banana', 'Orange'];
+
+app.get('/items', (req, res) => {
+    res.json(items);
+})
+
 //Serve static files from the "public" holder
 app.use(express.static('public'));
+
 
 app.post('/submit', (req, res) => {
     const data = req.body;
     res.send(`Received: ${JSON.stringify(data)}`);
 });
 
+app.post('/items', (req, res) => {
+    const newItem = req.body.item;
+    items.push(newItem);
+    res.json(items);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 //Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
